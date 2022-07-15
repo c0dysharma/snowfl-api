a47daa12c4f3b33c4bf93f673118ef50f317bdeb
Kuldeep <c0dysharma01@gmail.com>
2022-07-15 22:04:40 +0530

    crawling stage

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-const baseURl = 'https://snowfl.com/RIRCDbdSyEmeiBegmhFUyOfBboBuxSddD/';
-const isNsfw = {
-  yes : 1,
-  no : 0
-}
+const getApiKey = require('./lib/getApiKey');
+const axios = require('axios');
+const baseURl = 'https://snowfl.com/';
 
 class Snowfl {
-  constructor(query, sort = Snowfl.sortEnum.NONE) {
-    this.query = query;
-    this.sort = sort
-    this.buildUrl()
-  }
   static sortEnum = {
-    MAX_SEED: '/DH5kKsJw/0/SEED/NONE/1', // Sort based on more seeds
-    MAX_LEECH: '/DH5kKsJw/0/LEECH/NONE/1',
-    SIZE_ASC: '/DH5kKsJw/0/SIZE_ASC/NONE/1',
-    SIZE_DSC: '/DH5kKsJw/0/SIZE/NONE/1',
-    RECENT: '/DH5kKsJw/0/DATE/NONE/1',
-    NONE: '/DH5kKsJw/0/NONE/NONE/1'
+    MAX_SEED: '/DH5kKsJw/0/SEED/NONE/', // Sort based on more seeds
+    MAX_LEECH: '/DH5kKsJw/0/LEECH/NONE/',
+    SIZE_ASC: '/DH5kKsJw/0/SIZE_ASC/NONE/',
+    SIZE_DSC: '/DH5kKsJw/0/SIZE/NONE/',
+    RECENT: '/DH5kKsJw/0/DATE/NONE/',
+    NONE: '/DH5kKsJw/0/NONE/NONE/'
   };
 
-  buildUrl(query = this.query) {
+  async parse(query, { sort = Snowfl.sortEnum.NONE, isNsfw = false } = {}) {
+    this.query = query;
+    this.sort = sort;
+    this.isNsfw = isNsfw ? 1 : 0;
+    this.api = this.api ? this.api : await getApiKey();
+
     const sortEnum = Snowfl.sortEnum;
     if (Object.values(sortEnum).includes(this.sort))
-      this.url = `${baseURl}${this.query}${this.sort}`
+      this.url = `${baseURl}${this.api}/${this.query}${this.sort}${this.isNsfw}`
+    else this.url = `${baseURl}${this.api}/${this.query}${sortEnum.NONE}${this.isNsfw}`
 
-    else this.url = `${baseURl}${this.query}${sortEnum.NONE}`
+    const res = await axios.get(this.url);
+    return res.data;
   }
+
 }
-const sort = Snowfl.sortEnum
-module.exports = { Snowfl, sort };
+const Sort = Snowfl.sortEnum
+module.exports = { Snowfl, Sort };
