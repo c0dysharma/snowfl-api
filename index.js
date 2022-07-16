c2b860c88b9adab1ca95ff352d1b389b5e7beae1
Kuldeep <c0dysharma01@gmail.com>
2022-07-16 15:02:50 +0530

    Added comments

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const baseURl = 'https://snowfl.com/';
 
 class Snowfl {
+  // Optional args to sort data
   static sortEnum = {
     MAX_SEED: '/DH5kKsJw/0/SEED/NONE/',
     MAX_LEECH: '/DH5kKsJw/0/LEECH/NONE/',
@@ -20,16 +21,16 @@ class Snowfl {
     const sortEnum = Snowfl.sortEnum;
 
     try {
-      this.api = await getApiKey();
+      this.api = await getApiKey(); // get the apiKey from helper function
       if (this.api == null) throw error('Exiting')
 
-      if (Object.values(sortEnum).includes(this.sort))
+      if (Object.values(sortEnum).includes(this.sort)) // check if invalid sorting option
         this.url = `${baseURl}${this.api}/${this.query}${this.sort}${this.includeNsfw}`
-      else this.url = `${baseURl}${this.api}/${this.query}${sortEnum.NONE}${this.includeNsfw}`
+      else this.url = `${baseURl}${this.api}/${this.query}${sortEnum.NONE}${this.includeNsfw}` // revert to default- NONE
 
       const res = await axios.get(this.url);
       if (res.status != 200) throw Error("Couldn't get data");
-      return res.data;
+      return res.data; // returns Object data
 
     } catch (error) {
       console.error('Something Went wrong....')
