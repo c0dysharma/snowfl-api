3ddc38e3bc78a2b85b4804269a934cdc24e6c569
Kuldeep <c0dysharma01@gmail.com>
2022-07-16 14:24:40 +0530

    Added checks

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const getApiKey = require('./lib/getApiKey');
 const axios = require('axios');
+
 const baseURl = 'https://snowfl.com/';
 
 class Snowfl {
   static sortEnum = {
-    MAX_SEED: '/DH5kKsJw/0/SEED/NONE/', // Sort based on more seeds
+    MAX_SEED: '/DH5kKsJw/0/SEED/NONE/',
     MAX_LEECH: '/DH5kKsJw/0/LEECH/NONE/',
     SIZE_ASC: '/DH5kKsJw/0/SIZE_ASC/NONE/',
     SIZE_DSC: '/DH5kKsJw/0/SIZE/NONE/',
@@ -15,16 +16,25 @@ class Snowfl {
   async parse(query, { sort = Snowfl.sortEnum.NONE, includeNsfw = false } = {}) {
     this.query = query;
     this.sort = sort;
-    this.includeNsfw = typeof(includeNsfw) == "boolean" && includeNsfw ? 1 : 0;
-    this.api = this.api ? this.api : await getApiKey();
-
+    this.includeNsfw = typeof (includeNsfw) == "boolean" && includeNsfw ? 1 : 0;
     const sortEnum = Snowfl.sortEnum;
-    if (Object.values(sortEnum).includes(this.sort))
-      this.url = `${baseURl}${this.api}/${this.query}${this.sort}${this.includeNsfw}`
-    else this.url = `${baseURl}${this.api}/${this.query}${sortEnum.NONE}${this.includeNsfw}`
 
-    const res = await axios.get(this.url);
-    return res.data;
+    try {
+      this.api = await getApiKey();
+      if (this.api == null) throw error('Exiting')
+
+      if (Object.values(sortEnum).includes(this.sort))
+        this.url = `${baseURl}${this.api}/${this.query}${this.sort}${this.includeNsfw}`
+      else this.url = `${baseURl}${this.api}/${this.query}${sortEnum.NONE}${this.includeNsfw}`
+
+      const res = await axios.get(this.url);
+      if (res.status != 200) throw Error("Couldn't get data");
+      return res.data;
+
+    } catch (error) {
+      console.error('Something Went wrong....')
+      console.error(error);
+    }
   }
 
 }
